7bbc2f59cc781883bb147827742e0b1a8bcfbdc9
bethelbahiru <bethelbahiru@gmail.com>
2021-11-30 20:29:45 -0600

    initial commit

---
+++ src/ui/LibrarianWindow.java
@@ -0,0 +1,68 @@
+package ui;
+
+import business.ControllerInterface;
+import business.LoginException;
+import business.SystemController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class LibrarianWindow extends Stage implements LibWindow {
+	
+	public static final LibrarianWindow INSTANCE = new LibrarianWindow();
+	
+	private boolean isInitialized = false;
+	
+	public boolean isInitialized() {
+		return isInitialized;
+	}
+	public void isInitialized(boolean val) {
+		isInitialized = val;
+	}
+	private Text messageBar = new Text();
+	public void clear() {
+		messageBar.setText("");
+	}
+	
+	/* This class is a singleton */
+    private LibrarianWindow() {}
+    
+    public void init() { 
+        GridPane grid = new GridPane();
+        grid.setId("top-container");
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Checkout Book");
+        scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); //Tahoma
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Button librarianBtn = new Button("Checkout Book");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BASELINE_CENTER);
+        hbBtn.getChildren().add(librarianBtn);
+        grid.add(hbBtn, 1, 4);
+
+        Scene scene = new Scene(grid, 420, 375);
+        scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
+        setScene(scene);
+        
+    }
+
+
+
+}
